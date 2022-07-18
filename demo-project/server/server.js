081a047efb8ca3529307dbaff78396f18f71e3ba
teena thakur <teena.t@beehyv.com>
2022-07-18 21:22:22 +0530

    first commit

---
+++ demo-project/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const api = require('./routes/api');
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json()); 
+
+app.use('/api', api);
+
+app.get('/', function(req, res){
+  res.send('Hello from server');
+});
+
+app.listen(port, function(){
+    console.log("Server running on localhost:" + port);
+});
